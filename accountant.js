538db6dc850e30e767a9487ea62a995e40aa8635
Ilya Sharonov <ilia.sharonov@gmail.com>
2017-12-12 15:13:11 +0300

    Added accountant task

---
+++ accountant.js
@@ -0,0 +1,19 @@
+/*
+    An accountant is still unable to sum things up.
+    It's either an invalid result or even not a number.
+    Add some code so that the final sum will fit.
+    Mission is accomplished if console.log() at line #18 runs.
+    NOTE: it's prohibited to modify the code of the IIFE itself.
+ */
+
+(function accountant() {
+  const balance = [1,2,,4,5];
+  let sum = 0;
+  for (let i = 0; i < balance.length; i++) {
+    sum += balance[i];
+  }
+  if (sum !== 15) {
+    throw new Error(`Oh no! It's all wrong again!`);
+  }
+  console.log(`Yep! It's correct:-)`);
+})();
